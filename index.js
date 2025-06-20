9c633471cae594f2e4aa04538e5b1c7412144262
ADMIN <adityakhade1718@gmail.com>
2025-06-20 15:34:35 +0530

    first commit

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/verify', verifyUser, (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('server listening on port 3000')
+    console.log(`server listening on port ${port}`)
 })
 
 app.get('/', (req, res) => {
