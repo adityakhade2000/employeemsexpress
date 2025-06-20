784e7fad7c74adb216f613153e5ea667234890c2
ADMIN <adityakhade1718@gmail.com>
2025-06-20 16:16:57 +0530

    first commit

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 const client = new Client({
     host: process.env.DB_HOST,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
